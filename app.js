14e0b86d68aef7f8dc7fe0c1d36fdb0a8739290e
afattobene <adila.fattobene@ifood.com.br>
2021-05-19 22:57:27 -0300

    feat: atividade 8 - render tarsila database data

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ app.get("/", function (req, res) {
 });
 
 app.get("/tarsila", function (req, res) {
-  res.render("tarsila.ejs");
+  let connection = dbConnection();
+
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Conectado");
+  });
+
+  let sql = "select * from obrasdearte where artista = 'Tarsila do Amaral';";
+
+  connection.query(sql, function (err, result) {
+    res.render("tarsila.ejs", { obrasdearte: result });
+  });
 });
 
 app.get("/portinari", function (req, res) {
