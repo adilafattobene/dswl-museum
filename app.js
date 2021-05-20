244ef6fc2ff056551c024cec7efb5401ada92045
afattobene <adila.fattobene@ifood.com.br>
2021-05-19 23:08:37 -0300

    feat: atividade 8 - create fotos page

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const app = require("./config/server");
 const dbConnection = require("./config/dbServer");
 
 app.get("/", function (req, res) {
-  res.render("home.ejs");
+  let connection = dbConnection();
+
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Conectado");
+  });
+
+  let sql = "select * from fotos;";
+
+  connection.query(sql, function (err, result) {
+    res.render("fotos.ejs", { fotos: result });
+  });
 });
 
 app.get("/tarsila", function (req, res) {
