4135f0675fbd45d18e453aeb315067deb6b32ba0
afattobene <adila.fattobene@ifood.com.br>
2021-06-03 17:58:40 -0300

    release: change app to use routes file

--- app.js
+++ app.js
@@ -1,62 +1,7 @@
 const app = require("./config/server");
-const dbConnection = require("./config/dbServer");
+const routes = require("./app/routes/routes");
 
-app.get("/", function (req, res) {
-  let connection = dbConnection();
-
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Conectado");
-  });
-
-  let sql = "select * from fotos;";
-
-  connection.query(sql, function (err, result) {
-    res.render("fotos.ejs", { fotos: result });
-  });
-});
-
-app.get("/tarsila", function (req, res) {
-  let connection = dbConnection();
-
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Conectado");
-  });
-
-  let sql = "select * from obrasdearte where artista = 'Tarsila do Amaral';";
-
-  connection.query(sql, function (err, result) {
-    res.render("tarsila.ejs", { obrasdearte: result });
-  });
-});
-
-app.get("/portinari", function (req, res) {
-  let connection = dbConnection();
-
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Conectado");
-  });
-
-  let sql = "select * from obrasdearte where artista = 'Cândido Portinari';";
-
-  connection.query(sql, function (err, result) {
-    res.render("portinari.ejs", { obrasdearte: result });
-  });
-});
-
-app.get("/obrasdearte", function (req, res) {
-  let connection = dbConnection();
-
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Conectado");
-  });
-
-  let sql = "select * from obrasdearte";
-
-  connection.query(sql, function (err, result) {
-    res.render("home.ejs", { obrasdearte: result });
-  });
-});
+routes.home(app);
+routes.tarsila(app);
+routes.portinari(app);
+routes.obrasdearte(app);
