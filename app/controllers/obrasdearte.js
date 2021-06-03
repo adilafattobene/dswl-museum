5b7ab8b46f4b22f415ef0022f138f2b286924bd8
afattobene <adila.fattobene@ifood.com.br>
2021-06-03 18:11:51 -0300

    feat: add route and controller to getTarsila obras de arte

--- app/controllers/obrasdearte.js
+++ app/controllers/obrasdearte.js
@@ -14,3 +14,18 @@ module.exports.getAll = function (app, req, res) {
     res.render("home.ejs", { obrasdearte: result });
   });
 };
+
+module.exports.getTarsila = function (app, req, res) {
+  let connection = dbConnection();
+
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Conectado");
+  });
+
+  let sql = "select * from obrasdearte where artista = 'Tarsila do Amaral';";
+
+  connection.query(sql, function (err, result) {
+    res.render("tarsila.ejs", { obrasdearte: result });
+  });
+};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,5 @@
 const dbConnection = require("../../config/dbServer");
-const { getAll } = require("../controllers/obrasdearte");
+const { getAll, getTarsila } = require("../controllers/obrasdearte");
 
 module.exports = {
   home: function (app) {
@@ -21,19 +21,7 @@ module.exports = {
 
   tarsila: function (app) {
     app.get("/tarsila", function (req, res) {
-      let connection = dbConnection();
-
-      connection.connect(function (err) {
-        if (err) throw err;
-        console.log("Conectado");
-      });
-
-      let sql =
-        "select * from obrasdearte where artista = 'Tarsila do Amaral';";
-
-      connection.query(sql, function (err, result) {
-        res.render("tarsila.ejs", { obrasdearte: result });
-      });
+      getTarsila(app, req, res);
     });
   },
 
