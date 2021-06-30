02839e6e8bf6c1726204dcd270a87229fc2418e5
Adila Emanuelle Fattobene <adila.fattobene@ifood.com.br>
2021-06-30 20:59:49 -0300

    feat: add error validation and logger into fotos controller

--- app/controllers/fotos.js
+++ app/controllers/fotos.js
@@ -5,11 +5,19 @@ module.exports.getAllPhotos = function (app, req, res) {
   let connection = dbConnection();
 
   connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+      logger.log({ level: "error", message: err.message });
+
+      let error =
+        "<h1> Ocorreu algum erro com o servidor: </h1><h2>" + err.code + "<h2>";
+      res.status(500).send(error);
+    }
+
     console.log("Conectado");
   });
 
   getAllFotos(connection, function (err, result) {
+    logger.log({ level: "info", message: "Página fotos acessada." });
     res.render("fotos.ejs", { fotos: result });
   });
 };
