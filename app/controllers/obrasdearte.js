b8205ef97fc8ff8fe0f1a180c92f66e8c00a7504
Adila Emanuelle Fattobene <adila.fattobene@ifood.com.br>
2021-06-30 21:00:07 -0300

    feat: add error validation and logger into obrasdearte  controller

--- app/controllers/obrasdearte.js
+++ app/controllers/obrasdearte.js
@@ -5,15 +5,25 @@ const {
   getAllPortinari,
 } = require("../models/obrasdearte");
 
+const logger = require("../../config/logger");
+
 module.exports.getAll = function (app, req, res) {
   let connection = dbConnection();
 
   connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+      logger.log({ level: "error", message: err.message });
+
+      let error =
+        "<h1> Ocorreu algum erro com o servidor: </h1><h2>" + err.code + "<h2>";
+      res.status(500).send(error);
+    }
+
     console.log("Conectado");
   });
 
   getAllObrasDeArte(connection, function (err, result) {
+    logger.log({ level: "info", message: "Página obra de arte acessada." });
     res.render("home.ejs", { obrasdearte: result });
   });
 };
@@ -22,11 +32,19 @@ module.exports.getTarsila = function (app, req, res) {
   let connection = dbConnection();
 
   connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+      logger.log({ level: "error", message: err.message });
+
+      let error =
+        "<h1> Ocorreu algum erro com o servidor: </h1><h2>" + err.code + "<h2>";
+      res.status(500).send(error);
+    }
+
     console.log("Conectado");
   });
 
   getAllTarsila(connection, function (err, result) {
+    logger.log({ level: "info", message: "Página Tarsila acessada." });
     res.render("tarsila.ejs", { obrasdearte: result });
   });
 };
@@ -35,11 +53,19 @@ module.exports.getPortinari = function (app, req, res) {
   let connection = dbConnection();
 
   connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+      logger.log({ level: "error", message: err.message });
+
+      let error =
+        "<h1> Ocorreu algum erro com o servidor: </h1><h2>" + err.code + "<h2>";
+      res.status(500).send(error);
+    }
+
     console.log("Conectado");
   });
 
   getAllPortinari(connection, function (err, result) {
+    logger.log({ level: "info", message: "Página Portinari acessada." });
     res.render("portinari.ejs", { obrasdearte: result });
   });
 };
